79735360fe3d100d5f30eaafe0e62bad7388d895
mahaabdulla <mahaabdulla87@gmail.com>
2025-06-10 19:28:50 +0300

    add cubit function

--- lib/cubit/task_cubit.dart
+++ lib/cubit/task_cubit.dart
@@ -1,9 +1,18 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/model/task.dart';
 
 part 'task_state.dart';
 
 class TaskCubit extends Cubit<TaskState> {
   TaskCubit() : super(TaskInitial());
+
+  void addTask(TaskModel title) {
+    List<TaskModel> task;
+    // task.map((e) => e.title,).toList();
+    //  emit(UpdateTask(task));
+  }
+
+  void deleteTask(int id) {}
+  void toggeleTask(bool isCompleted) {}
 }
--- lib/cubit/task_state.dart
+++ lib/cubit/task_state.dart
@@ -2,13 +2,16 @@ part of 'task_cubit.dart';
 
 sealed class TaskState extends Equatable {
   final List<TaskModel> taskList;
-
   const TaskState(this.taskList);
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [taskList];
 }
 
 final class TaskInitial extends TaskState {
-  TaskInitial(super.taskList);
+  TaskInitial() : super([]);
+}
+
+final class UpdateTask extends TaskState {
+  const UpdateTask(super.taskList);
 }
